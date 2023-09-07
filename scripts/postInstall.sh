59d31224e587185d743de7efcb1d621bf54e7477
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-07 08:40:30 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -8,7 +8,7 @@ sleep 30s;
 
 docker-compose exec -T mcs1 bash -c "provision mcs1"
 
-sed -i "s~\# - ./scripts/columnstore-init:/usr/bin/columnstore-init:rw~- ./scripts/columnstore-init:/usr/bin/columnstore-init:rw~g" ./docker-compose.yml
+# sed -i "s~\# - ./scripts/columnstore-init:/usr/bin/columnstore-init:rw~- ./scripts/columnstore-init:/usr/bin/columnstore-init:rw~g" ./docker-compose.yml
 
-docker-compose down;
-docker-compose up -d;
+# docker-compose down;
+# docker-compose up -d;
